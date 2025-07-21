c4cba1d58eb8fe9213ca0a0772e21da92bea19a7
Abdurrehman <abdurrehman.malik2006@gmail.com>
2025-07-21 17:47:19 +0500

    bug fixes

--- utils/predictor.py
+++ utils/predictor.py
@@ -2,9 +2,6 @@ import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
 
-def load_model():
-    model = tf.keras.models.load_model("model/handwritten.h5")
-    return model
 
 def predict_digit(image, model):
     image = ImageOps.grayscale(image)
@@ -13,14 +10,10 @@ def predict_digit(image, model):
     img = img / 255
     img = img.reshape((1, 28, 28, 1))
     pred = model.predict(img)
+    for i, p in enumerate(pred[0]):
+        print(f"Class {i}: {p:.2f}")
     result = np.argmax(pred[0])
     confidence = float(np.max(pred[0]) * 100)  # Convert to percentage
-    
-    # Convert 0-9 to 1-10 range
-    if result == 0:
-        display_result = 10  # Map 0 to 10
-    else:
-        display_result = result  # 1-9 stay the same
-    
-    print(f"Model prediction: {result}, Display result: {display_result}, Confidence: {confidence:.2f}%")
-    return int(display_result), confidence  # Ensure it's an integer
+
+    print(f"Model prediction: {result}, Display result: {result}, Confidence: {confidence:.2f}%")
+    return int(result), confidence
